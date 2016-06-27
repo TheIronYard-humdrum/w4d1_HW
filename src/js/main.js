4519ff0edc6fa56622caae301101da95dd2a4349
humdrum <jjrajani@gmail.com>
2016-06-27 16:06:38 -0400

    styling

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,10 @@ import $ from 'jquery'
 var logIt = function(data) {
   console.log("You've got a bug!" + data)
 }
+
 var run = function(results) {
   results.forEach(function(item) {
-    if (item.type != "select") {
+    if (item.type != "select" && item.type != "textarea") {
     var htmlString = `<div class="input">
                         <i class="fa ${item.icon}" aria-hidden="true"></i>
                         <label for="${item.id}">${item.label}
@@ -14,8 +15,17 @@ var run = function(results) {
                       </div>`
     $('#form').append(htmlString)
     }
+
+    if (item.type === "textarea") {
+      var textareaString = `<div class="textarea">
+                              <i class="fa ${item.icon} aria-hidden="true"></i>
+                              <label for="${item.id}">${item.label}<textarea id="${item.id}"></textarea>
+                            </div>`
+      $('#form').append(textareaString)
+    }
+
     if (item.options.length > 0) {
-      var optionString = `<div class="select"><select>`
+      var optionString = `<div class="select"><select><option value="Select your language...">Select your language...</option>`
       item.options.forEach(function(opt) {
         optionString += `<option value="opt.value">${opt.label}</option>`
       })
